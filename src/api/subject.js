3eb6a55e51b6695570f62d1168b05b82bba7f294
Kasnars <kasnars@163.com>
2022-02-15 09:51:26 +0000

    update src/api/subject.js.

--- src/api/subject.js
+++ src/api/subject.js
@@ -1,5 +1,29 @@
-import { fetchPostQuery } from "@/tools/httpTools"
+import { fetchPostBody, fetchPostQuery } from "@/tools/httpTools"
 
 export const getAllSubjectsHttp = (data) => {
     return fetchPostQuery('/subject/findAllSubjects', data)
+}
+
+export const addSubjectHttp = (body) => {
+    return fetchPostBody('/subject/addSubject', body)
+}
+
+export const deleteSubjectHttp = (query) => {
+    return fetchPostQuery('/subject/deleteSubject', query)
+}
+
+export const enableSubjectHttp = (query) => {
+    return fetchPostQuery('/subject/enableSubject', query)
+}
+
+export const findSubjectHttp = (query) => {
+    return fetchPostQuery('/subject/findSubject', query)
+}
+
+export const forbiddenSubject = (query) => {
+    return fetchPostQuery('/subject/forbiddenSubject', query)
+}
+
+export const updateSubject = () => {
+    return fetchPostBody('/subject/updateSubject', body)
 }
