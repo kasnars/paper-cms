8534cbebfdd3145e3b89804b04a2afb594851f44
Kasnars <kasnars@163.com>
2022-02-15 09:50:53 +0000

    update src/api/question.js.

--- src/api/question.js
+++ src/api/question.js
@@ -1,5 +1,33 @@
-import { fetchPostQuery } from "@/tools/httpTools"
+import { fetchPostAll, fetchPostBody, fetchPostQuery } from "@/tools/httpTools"
 
 export const getAllQuestionHttp = (data) => {
     return fetchPostQuery('/question/findAllQuestion', data)
+}
+
+export const addQuestionHttp = (body) => {
+    return fetchPostBody('/question/addQuestion', body)
+}
+
+export const deleteQuestionHttp = (query) => {
+    return fetchPostQuery('/question/deleteQuestion', query)
+}
+
+export const disableQuestionHttp = (query) => {
+    return fetchPostQuery('/question/disableQuestion', query)
+}
+
+export const enableQuestionHttp = (query) => {
+    return fetchPostQuery('/question/enableQuestion', query)
+}
+
+export const updateQuestionHttp = (body) => {
+    return fetchPostBody('/question/updateQuestion', body)
+}
+
+export const findQuestionByQuestionCodeHttp = (query) => {
+    return fetchPostQuery('/question/findQuestionByQuestionCode', query)
+}
+
+export const findQuestionHttp = (query, body) => {
+    return fetchPostAll('/question/findAllQuestion', query, body)
 }
