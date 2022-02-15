a69d64f2c94ff76f5af9233c92f1ee2d5dd13ce8
Kasnars <kasnars@163.com>
2022-02-15 09:54:49 +0000

    update src/views/questionManage/detail.vue.

---
+++ src/views/questionManage/detail.vue
@@ -0,0 +1,24 @@
+<template>
+<view>
+  {{id}}
+</view>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            id:''
+        }
+    },
+    mounted(){
+        this.id = this.$route.params.id
+        console.log(this.id);
+    }
+}
+</script>
+
+<style>
+
+</style>
