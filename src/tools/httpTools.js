a5785d3f75bc866197fc8ff10fb08bbf792dc96f
Kasnars <kasnars@163.com>
2022-02-15 09:54:29 +0000

    update src/tools/httpTools.js.

--- src/tools/httpTools.js
+++ src/tools/httpTools.js
@@ -7,11 +7,22 @@ export const fetchPostQuery = (url, query) => {
     Object.keys(query).map(item => {
         postQuery += `${item}=${query[item]}&`
     })
-    const postUrl = `${baseUrl}${url}?${postQuery}`
+    let postUrl = `${baseUrl}${url}?${postQuery}`
+    postUrl = postUrl.substring(0, postUrl.length-1)
     return axios.post(postUrl)
 }
 
 export const fetchPostBody = (url, body) => {
     const postUrl = `${baseUrl}${url}`
+    return axios.post(postUrl, body)
+}
+
+export const fetchPostAll = (url, query, body) => {
+    let postQuery = ''
+    Object.keys(query).map(item => {
+        postQuery += `${item}=${query[item]}&`
+    })
+    const postUrl = `${baseUrl}${url}?${postQuery}`
+    postUrl = postUrl.substring(0, postUrl.length - 1)
     return axios.post(postUrl, body)
 }
