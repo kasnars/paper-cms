4ff5cbcf01adae5331bf896a2708c5d668151d76
Kasnars <kasnars@163.com>
2022-02-21 10:01:06 +0000

    update src/views/subjectManage/index.vue.

--- src/views/subjectManage/index.vue
+++ src/views/subjectManage/index.vue
@@ -1,91 +1,40 @@
 <template>
   <div class="app-container">
-    <el-row :gutter="20">
-      <el-col :span="20">
-        <el-input
-          placeholder="请输入课程名称搜索"
-          prefix-icon="el-icon-search"
-          v-model="searchData"
-          @keyup.enter.native="searchHttp"
-        >
-        </el-input>
-      </el-col>
-      <el-col :span="2" >
-        <el-button type="primary" @click="searchHttp"
-          >点击搜索</el-button
-        >
-      </el-col>
-      <el-col :span="2" >
-        <el-button type="primary" @click="dialogFormVisible = true"
-          >新增课程</el-button
-        >
-      </el-col>
-    </el-row>
-
-    <el-dialog title="新增课程详情" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-form-item label="课程号" :label-width="formLabelWidth">
-          <el-input v-model="form.code" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="课程名" :label-width="formLabelWidth">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addHttp"
-          >确 定</el-button
-        >
-      </div>
-    </el-dialog>
-
-    <el-table :data="tableData" stripe style="width: 100%">
-      <!-- <el-table-column prop="id" label="ID" width="180"> </el-table-column> -->
-      <el-table-column prop="code" label="编号" width="180"> </el-table-column>
-      <el-table-column prop="name" label="课程名称" >
-      </el-table-column>
-      <el-table-column prop="statusText" label="状态" width="180"> </el-table-column>
-      <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.row)">修改</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.row)">删除</el-button>
-      </template>
+  <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="id"
+      label="ID"
+      width="180">
     </el-table-column>
-    </el-table>
-
-      <el-dialog title="修改课程详情" :visible.sync="editShow">
-      <el-form :model="editForm">
-        <el-form-item label="课程号" :label-width="formLabelWidth">
-          <el-input v-model="editForm.code" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="课程名" :label-width="formLabelWidth">
-          <el-input v-model="editForm.name" autocomplete="off"></el-input>
-        </el-form-item>
-
-                <el-form-item label="课程状态" :label-width="formLabelWidth">
-            <el-select v-model="editForm.status" placeholder="请选择">
-    <el-option label="启用" :value="true"></el-option>
-    <el-option label="禁用" :value="false"></el-option>
-  </el-select>
-        </el-form-item>
-
-      </el-form>
-
-
-
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="editShow = false">取 消</el-button>
-        <el-button type="primary" @click="editHttp"
-          >确 定</el-button
-        >
-      </div>
-    </el-dialog>
-        <el-row :gutter="20">
+    <el-table-column
+      prop="level"
+      label="等级"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="phone"
+      label="手机号"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="status"
+      label="状态"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="email"
+      label="邮箱">
+    </el-table-column>
+  </el-table>
+          <el-row :gutter="20">
       <el-col :span="12" :offset="6"
         ><div class="grid-content bg-purple">
 
@@ -113,129 +62,58 @@
 </template>
 
 <script>
-import { getList } from "@/api/table";
-import { addSubjectHttp, deleteSubjectHttp, findSubjectHttp, getAllSubjectsHttp, updateSubjectHttp } from "@/api/subject";
+import { getList } from '@/api/table'
+import { getAllUserListHttp } from '@/api/user'
 
 export default {
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: "success",
-        draft: "gray",
-        deleted: "danger",
-      };
-      return statusMap[status];
-    },
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
   },
   data() {
     return {
       list: null,
       listLoading: true,
-      fetchBody: {
-        currentPage: 1,
-        pageSize: 10,
+      fetchBody:{
+          currentPage:1,
+          pageSize:10
       },
-      tableData: [],
-      searchData: "",
-      dialogFormVisible: false,
-      formLabelWidth:'auto',
-      form: {
-        code:'',
-        name:'',
-      },
-      testData:[
-        {
-          id:'23233',
-          code:123123,
-          name:'测试',
-          status:false,
-        }
-      ],
-      editShow:false,
-      editForm:{
-
-      }
-    };
+      tableData:[]
+    }
   },
   created() {
-    this.fetchData();
+    this.fetchData()
   },
-  mounted() {
-    this.initData();
-    // this.tableData = this.testData
+  mounted(){
+    this.initData()
   },
   methods: {
     fetchData() {
-      this.listLoading = true;
-      getList().then((response) => {
-        this.list = response.data.items;
-        this.listLoading = false;
-      });
+      this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
     },
     initData() {
-      console.log(this.fetchBody);
-      getAllSubjectsHttp(this.fetchBody).then((res) => {
-        console.log(res, "res");
-        this.tableData = res.data.data;
-        console.log(this.tableData);
-        this.tableData.forEach((item) => {
-          item.statusText = item.status ? "有效" : "失效";
-        });
-      });
-    },
-    addHttp(){
-      console.log(this.form,'form');
-      addSubjectHttp(this.form).then(() => {
-        this.initData()
-        this.form = {}
-      })
-      this.dialogFormVisible = false
-    },
-    searchHttp(){
-      let payload = { ...this.fetchBody, name:this.searchData}
-      console.log(this.searchData);
-      findSubjectHttp(payload).then(res => {
-        console.log(res.data.data);
-        this.tableData = res.data.data
-      })
-    },
-    handleEdit(row){
-      this.editForm = JSON.parse(JSON.stringify(row))
-      this.editShow = true
-      console.log(row);
-
-    },
-    handleDelete(row){
-      const { id } =  row 
-      console.log(id,'del');
-       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          deleteSubjectHttp({subjectId: id}).then(() => {
-            this.initData()
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          });
-          })
-
-        }).catch(() => {
-            this.$message({
-            type: 'info',
-            message: '操作已取消!'
-          });
+        let query = `currentPage=${this.fetchBody.currentPage}&pageSize=${this.fetchBody.pageSize}`
+        console.log(this.fetchBody);
+        getAllUserListHttp(this.fetchBody).then(res => {
+            console.log(res,'res');
+            this.tableData = res.data.data
+            console.log(this.tableData);
+            this.tableData.forEach(item => {
+                item.status = item.status?'有效':'失效'
+            })
         })
     },
-    editHttp(){
-      console.log(this.editForm,'edithttp');
-      updateSubjectHttp(this.editForm).then(() => {
-        this.initData()
-        this.editShow = false
-      })
-    },
-        nextPage() {
+            nextPage() {
       this.fetchBody.currentPage++;
       this.initData();
     },
@@ -243,6 +121,6 @@ export default {
       this.fetchBody.currentPage--
       this.initData()
     }
-  },
-};
+  }
+}
 </script>
